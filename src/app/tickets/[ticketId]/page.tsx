3460275db9ee553497eecb7966c76a703b65c21f
Raman7913 <raman7913523@gmail.com>
2025-02-08 18:30:20 +0545

    temporarily disabled validation of types

--- src/app/tickets/[ticketId]/page.tsx
+++ src/app/tickets/[ticketId]/page.tsx
@@ -1,24 +1,38 @@
 import React from "react";
 import inititalTickets from "@/data";
 
-type ticketPageProps = {
+// Define the type for the ticket
+type Ticket = {
+    id: string;
+    title: string;
+    content: string;
+};
+
+// Define the props for the page component
+interface TicketPageProps {
     params: {
         ticketId: string;
     };
-};
-const TicketPage = ({ params }: ticketPageProps) => {
+}
+
+const TicketPage = ({ params }: TicketPageProps) => {
+    // Find the ticket based on the ticketId
     const ticketInfo = inititalTickets.find(
         (ticket) => ticket.id === params.ticketId
     );
-    if (ticketInfo === undefined) {
-        return "ticket not found";
+
+    // If the ticket is not found, return a message
+    if (!ticketInfo) {
+        return <div>Ticket not found</div>;
     }
+
+    // Render the ticket details
     return (
         <div>
             <h1 className="text-red-200 text-8xl text-center">
-                {ticketInfo?.title}
+                {ticketInfo.title}
             </h1>
-            <h2>{ticketInfo?.content}</h2>
+            <h2>{ticketInfo.content}</h2>
         </div>
     );
 };
